54c67d2d0ffeaef93b7cd9d65cf96816b92b00b4
anushri kumari <anushrikumariatps592007@gmail.com>
2026-09-17 08:00:30 +0530

    AdminDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Login from './components/Auth/login'
 import EmployeeDsahboard from './components/Dashboard/EmployeeDsahboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <EmployeeDsahboard />
+      {/* <EmployeeDsahboard /> */}
+      <AdminDashboard />
     </>
   )
 }
--- src/components/Dashboard/EmployeeDsahboard.jsx
+++ src/components/Dashboard/EmployeeDsahboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskListNumber from '../other/TaskListNumber'
+import TaskList from '../Task List/TaskList'
 const EmployeeDsahboard = () => {
   return (
-    <div className='p-10  bg-[#1C1C1C]' h-screen>
+    <div className='p-10  bg-[#1C1C1C] h-screen'>
     <Header />
     <TaskListNumber />
+    <TaskList />
     </div>
   )
 }
